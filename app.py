b60db711aef9bf76736c225bd150fc13bc097e20
VALENZUELA ACOSTA MARIO ENRIQUE <mario.valenzuela@ine.mx>
2026-03-17 11:57:12 -0700

    Acutalizado con ciclos

--- app.py
+++ app.py
@@ -9,17 +9,42 @@ def obtener_jugada():
             return jugador
         else:
             print("Opcion Invalida.")
+
 jugador = obtener_jugada()
 computadora = random.choice(opciones)
+victorias_jugador = 0
+victorias_computadora = 0
+while True:
+    print("La Computadora eligió:", computadora)
+    if jugador == computadora:
+        print("¡Empate!")
+    elif jugador == "piedra" and computadora == "tijeras":
+        print("¡Ganaste! La piedra aplasta las tijeras.")
+        victorias_jugador += 1
+    elif jugador == "papel" and computadora == "piedra":
+        print("¡Ganaste! El papel envuelve la piedra.")
+        victorias_jugador += 1
+    elif jugador == "tijeras" and computadora == "papel":
+        print("¡Ganaste! Las tijeras cortan el papel.")
+        victorias_jugador += 1
+    else:
+        print("¡Perdiste! La computadora gana.")
+        victorias_computadora += 1
 
-print("La Computadora eligió:", computadora)
-if jugador == computadora:
-    print("¡Empate!")
-elif jugador == "piedra" and computadora == "tijeras":
-    print("¡Ganaste! La piedra aplasta las tijeras.")
-elif jugador == "papel" and computadora == "piedra":
-    print("¡Ganaste! El papel envuelve la piedra.")
-elif jugador == "tijeras" and computadora == "papel":
-    print("¡Ganaste! Las tijeras cortan el papel.")
-else:
-    print("¡Perdiste! La computadora gana.")
+    print("Puntuación del jugador:", victorias_jugador)
+    print("Puntuación de la computadora:", victorias_computadora)
+
+    respuesta = input("¿Quieres volver a jugar? Sí/No: ").lower()
+    if respuesta == "sí" or respuesta == "si":
+        jugador = obtener_jugada()
+        computadora = random.choice(opciones) 
+    else: 
+        print("¡Gracias por jugar! ¡Hasta luego!")
+
+        if victorias_jugador > victorias_computadora:
+            print("El ganador de esta ronda fue: El jugador con", victorias_jugador)
+        elif victorias_computadora > victorias_jugador:
+            print("El ganador de esta ronda fue: La computadora con", victorias_computadora)
+        else:
+            print("¡La ronda terminó en empate con", victorias_jugador, "victorias cada uno!")
+        break
